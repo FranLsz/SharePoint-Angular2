4b8ce83d68393722db104f97c93b2371c2e93c74
Fran <franlsz95develop@gmail.com>
2016-04-05 01:23:11 +0200

    Base funcionando v2

--- SpAngular2AddIn/Scripts/angular2/app/main.ts
+++ SpAngular2AddIn/Scripts/angular2/app/main.ts
@@ -3,6 +3,6 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppComponent} from './components/app.component'
 
-const COMPONENT_BASE_PATH = './app/components/todos';
+const BASE_PATH = '[URL]';
 
 bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
